66e9ed6d78b182d214a9a94bae00be3d38d0c664
hascdev <ale.sandoval.ii@gmail.com>
2025-09-02 14:59:21 -0400

    feat: update README and add client script for document validation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 2win_andes_doc_compra_duplicado
+
+- Requisitos previos a la instalación
+
+    1. Bundle 2win_facturacion_modulo_modelo (536988)
+
+- Requisitos posteriores a la instalación
+
+    No hay
+    
+- Funcionalidades
+
+    1. [2Win CS Valida Duplicado Doc. Compra](src/FileCabinet/SuiteScripts/2win_andes_doc_compra_duplicado/2win_cs_valida_duplicado_doc_compra.js): Script Cliente que se utiliza para validar documentos en el evento saveRecord de la transacción.
+
+- Tablas y Campos
+
+    1. transaction (VendorBill y VendorCredit)
+        - custbody_tipodocumentoelectronico
+
+    2. vendor
+        - custentity_2wrut
--- src/FileCabinet/SuiteScripts/2win_andes_doc_compra_duplicado/domain/2win_dom_doc_compra.js
+++ src/FileCabinet/SuiteScripts/2win_andes_doc_compra_duplicado/domain/2win_dom_doc_compra.js
@@ -16,14 +16,14 @@ define(['N/record', '../dao/2win_dao_doc_compra'],
                 log.audit("existeDocumento - facturas_afectas", facturas_afectas.length);
 
                 if (facturas_afectas.length > 0) {
-                    return "Ya existe la factura afecta id " + facturas_afectas[0].id + " con el mismo tipo de documento, RUT proveedor y folio.";
+                    return "Ya existe la <b>Factura Afecta ID " + facturas_afectas[0].id + "</b> con el mismo tipo de documento, RUT proveedor y folio.";
                 }
 
                 var facturas_exentas = dao_doc_compra.obtenerFacturaExenta(id_tipo, rut_proveedor, folio);
                 log.audit("existeDocumento - facturas_exentas", facturas_exentas.length);
 
                 if (facturas_exentas.length > 0) {
-                    return "Ya existe la factura exenta id " + facturas_exentas[0].id + " con el mismo tipo de documento, RUT proveedor y folio.";
+                    return "Ya existe la <b>Factura Exenta ID " + facturas_exentas[0].id + "</b> con el mismo tipo de documento, RUT proveedor y folio.";
                 }
 
                 // Buscar boleta de honorario
@@ -31,7 +31,7 @@ define(['N/record', '../dao/2win_dao_doc_compra'],
                 log.audit("existeDocumento - boletas_honorario", boletas_honorario.length);
 
                 if (boletas_honorario.length > 0) {
-                    return "Ya existe la boleta de honorario id " + boletas_honorario[0].id + " con el mismo RUT proveedor y folio.";
+                    return "Ya existe la <b>Boleta de Honorario ID " + boletas_honorario[0].id + "</b> con el mismo RUT proveedor y folio.";
                 }
 
                 return null;
